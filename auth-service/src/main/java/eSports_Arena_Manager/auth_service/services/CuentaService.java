f37fe6aad8068f42d63fa930e94fa112fca96bac
Jose <jj.hernandez@duocuc.cl>
2026-05-12 18:39:36 -0400

    se agrega cuentaService y CuentaServiceImpl

---
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/services/CuentaService.java
@@ -0,0 +1,16 @@
+package eSports_Arena_Manager.auth_service.services;
+
+import eSports_Arena_Manager.auth_service.models.Cuenta;
+
+import java.util.List;
+
+public interface CuentaService {
+    List<Cuenta> findAll();
+    Cuenta findById(Long id);
+    Cuenta findByCorreo(String correo);
+    Cuenta save(Cuenta cuenta);
+    Cuenta updateById(Long id, Cuenta cuenta);
+    void deleteById(Long id);
+    Cuenta findByUsuario(String usuario);
+
+}
