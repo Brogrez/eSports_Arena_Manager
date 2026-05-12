259e4b934458d15627ceb3e0050718551d436673
Jose <jj.hernandez@duocuc.cl>
2026-05-12 18:09:41 -0400

    Se crea los packages, modelo Audit y Cuenta, ademas del Exception

---
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/models/Cuenta.java
@@ -0,0 +1,59 @@
+package eSports_Arena_Manager.auth_service.models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "cuentas")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Cuenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cuentaId;
+
+    @NotBlank(message = "El campo de correo no puede ser vacio")
+    @Email(message = "El campo de correo tiene que tener el formato de correo")
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+
+    @NotBlank(message = "El campo de contraseña no puede ser vacio")
+    @Column(nullable = false)
+    private String contrasenia;
+
+    @NotBlank(message = "tiene que haber un rol asignado")
+    @Column(nullable = false)
+    private String rol;
+
+    @NotBlank(message = "tiene que haber un estado asignado")
+    @Column(nullable = false)
+    private String estado;
+
+    @NotNull(message = "El campo de fecha de creacion no puede ser vacio")
+    @Column(nullable = false)
+    private LocalDateTime fechaCreacion;
+
+
+    @Embedded
+    private Audit audit = new Audit();
+
+
+
+
+}
