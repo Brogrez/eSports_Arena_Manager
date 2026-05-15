df25256c5c17ad2f167dde5548d6e5a51ea50016
José <jj.hernandez@duocuc.cl>
2026-05-15 12:01:42 -0400

    se crea EquipoRepository implementando sus metodos

---
+++ team-service/src/main/java/com/duoc/team_service/repositories/EquipoRepository.java
@@ -0,0 +1,15 @@
+package com.duoc.team_service.repositories;
+
+import com.duoc.team_service.models.Equipo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EquipoRepository extends JpaRepository<Equipo,Long> {
+    Optional<Equipo> findByJuegoPrincipalId(Long juegoId);
+    Optional<Equipo> findByCapitanId(Long capitanId);
+    Optional<Equipo> findByEstado(String estado);
+}
