9e2fe9656b7e37854dc89cfb3e6056801395528f
Nicolas <nico.mv1998@gmail.com>
2026-05-15 11:59:33 -0400

    creando repository y sus metodos opcionales

---
+++ game-service/src/main/java/eSports_Arena_Manager/game_service/repositories/GameRepository.java
@@ -0,0 +1,13 @@
+package eSports_Arena_Manager.game_service.repositories;
+
+import eSports_Arena_Manager.game_service.models.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    Optional<Game> findByGenero(String genero);
+    Optional<Game> findByModalidad(String modalidad);
+    Optional<Game> findByName(String name);
+    Optional<Game> findByEstado(String estado);
+}
