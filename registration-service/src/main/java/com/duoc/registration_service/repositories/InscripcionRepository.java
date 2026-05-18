93f52fe1edcd5f1912b048cdca8c08d7def064e1
Jose <jj.hernandez@duocuc.cl>
2026-05-18 17:42:56 -0400

    se cambia de List a Optional para hacer consulta directa mediante ID

--- registration-service/src/main/java/com/duoc/registration_service/repositories/InscripcionRepository.java
+++ registration-service/src/main/java/com/duoc/registration_service/repositories/InscripcionRepository.java
@@ -4,13 +4,13 @@ import com.duoc.registration_service.models.Inscripcion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 import java.util.Optional;
 
 @Repository
 public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
-    List<Inscripcion> findByTorneoId(Long torneoId);
-    List<Inscripcion> findByEquipoId(Long equipoId);
-    List<Inscripcion> findByJugadorId(Long jugadorId);
+    Optional<Inscripcion> findByTorneoId(Long torneoId);
+    Optional<Inscripcion> findByEquipoId(Long equipoId);
+    Optional<Inscripcion> findByJugadorId(Long jugadorId);
     Optional<Inscripcion> findByTorneoIdAndJugadorId(Long torneoId, Long jugadorId); //para  evitar duplicados
 }
