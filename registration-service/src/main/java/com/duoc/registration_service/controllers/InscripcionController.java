69be46e1a42dd3af8738d215bc88dbf180c4b95b
Jose <jj.hernandez@duocuc.cl>
2026-05-18 18:16:03 -0400

    Se realiza controller

---
+++ registration-service/src/main/java/com/duoc/registration_service/controllers/InscripcionController.java
@@ -0,0 +1,65 @@
+package com.duoc.registration_service.controllers;
+
+import com.duoc.registration_service.models.Inscripcion;
+import com.duoc.registration_service.services.InscripcionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/inscripciones")
+@Validated
+public class InscripcionController {
+
+    @Autowired
+    private InscripcionService inscripcionService;
+
+
+    //crear inscripcion
+    @PostMapping
+    public ResponseEntity<Inscripcion> save(@RequestBody Inscripcion inscripcion) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(inscripcionService.save(inscripcion));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Inscripcion>> findAll()  {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(inscripcionService.findAll());
+    }
+
+    @GetMapping("/torneo/{torneoId}")
+    public ResponseEntity<List<Inscripcion>> findByTorneoId(@PathVariable Long torneoId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(List.of(inscripcionService.findByTorneoId(torneoId)));
+    }
+
+    @GetMapping("/jugador/{jugadorId}")
+    public ResponseEntity<List<Inscripcion>> findByJugadorId(@PathVariable Long jugadorId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(List.of(inscripcionService.findByJugadorId(jugadorId)));
+    }
+    @GetMapping("/equipo/{equipoId}")
+    public ResponseEntity<List<Inscripcion>> findByEquipoId(@PathVariable Long equipoId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(List.of(inscripcionService.findByEquipoId(equipoId)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        inscripcionService.deleteById(id);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+}
