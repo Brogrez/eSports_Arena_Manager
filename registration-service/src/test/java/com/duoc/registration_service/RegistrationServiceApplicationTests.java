acc36f8e2b8321271b792eee8038a3d25a1663d6
Jose <jj.hernandez@duocuc.cl>
2026-05-18 16:46:32 -0400

    se agrega rama registration-service y se agrega a h2 con puerto 8005

---
+++ registration-service/src/test/java/com/duoc/registration_service/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
