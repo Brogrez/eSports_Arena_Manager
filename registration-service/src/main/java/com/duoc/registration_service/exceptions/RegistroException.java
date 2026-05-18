f213bc9db8c49c11c74da166c4a77b757a89747f
Jose <jj.hernandez@duocuc.cl>
2026-05-18 17:00:24 -0400

    se agrega RegistroException

---
+++ registration-service/src/main/java/com/duoc/registration_service/exceptions/RegistroException.java
@@ -0,0 +1,7 @@
+package com.duoc.registration_service.exceptions;
+
+public class RegistroException extends RuntimeException {
+    public RegistroException(String message) {
+        super(message);
+    }
+}
