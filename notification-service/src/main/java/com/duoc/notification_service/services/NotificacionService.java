5db27b07fb1360dd669ccc953e362c102e55ec91
Jose Hernandez <jj.hernandez@duocuc.cl>
2026-05-19 11:22:50 -0400

    se agrega funciones faltantes como Save, delete, marcar como leido y el update

--- notification-service/src/main/java/com/duoc/notification_service/services/NotificacionService.java
+++ notification-service/src/main/java/com/duoc/notification_service/services/NotificacionService.java
@@ -5,9 +5,13 @@ import com.duoc.notification_service.models.Notificacion;
 import java.util.List;
 
 public interface NotificacionService {
-    List<Notificacion> findByUsuarioId(Long usuarioId);
-    List<Notificacion> findByEquipoId(Long equipoId);
+
+    Notificacion findByUsuarioId(Long usuarioId);
+    Notificacion findByEquipoId(Long equipoId);
     List<Notificacion> findAll();
     Notificacion findBynotificacionId(Long id);
-
+    void deleteBynotificacionId(Long id);
+    Notificacion save(Notificacion notificacion);
+    Notificacion update(Notificacion notificacion);
+    Notificacion marcarComoLeido(Long id);
 }
