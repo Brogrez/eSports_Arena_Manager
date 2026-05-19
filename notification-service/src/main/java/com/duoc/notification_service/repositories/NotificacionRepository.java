6f6aa12e8208480fa1fe416fa016dbe4b7412efb
Jose Hernandez <jj.hernandez@duocuc.cl>
2026-05-19 11:22:10 -0400

    se cambia de List a Optional

--- notification-service/src/main/java/com/duoc/notification_service/repositories/NotificacionRepository.java
+++ notification-service/src/main/java/com/duoc/notification_service/repositories/NotificacionRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NotificacionRepository extends JpaRepository<Notificacion,Long> {
-    List<Notificacion> findByUsuarioId(Long usuarioId);
-    List<Notificacion> findByEquipoId(Long equipoId);
-
+    Optional<Notificacion> findByUsuarioId(Long usuarioId);
+    Optional<Notificacion> findByEquipoId(Long equipoId);
+    Optional<Notificacion> findByNotificacionId(Long id);
 }
