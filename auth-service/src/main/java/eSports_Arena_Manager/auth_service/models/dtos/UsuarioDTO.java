ccc7a9d2152b179246bebb69aca981d961e48a63
Jose <jj.hernandez@duocuc.cl>
2026-05-22 20:51:30 -0400

    se agrega UsuarioDTO con sus datos correspondientes

---
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/models/dtos/UsuarioDTO.java
@@ -0,0 +1,14 @@
+package eSports_Arena_Manager.auth_service.models.dtos;
+
+import java.time.LocalDate;
+
+public class UsuarioDTO {
+
+    private Long idUsuario;
+    private String rol;
+    private String estado;
+    private LocalDate fechaRegistro;
+    private String correo;
+    private String nickname;
+
+}
