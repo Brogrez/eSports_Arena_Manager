a2bbdcfe75947553584a112b18722b5385c50a47
Jose <jj.hernandez@duocuc.cl>
2026-05-22 20:51:12 -0400

    .

--- auth-service/src/main/java/eSports_Arena_Manager/auth_service/clients/UsuarioClient.java
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/clients/UsuarioClient.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "user-service", url = "localhost:8000/api/v1/users")
 public interface UsuarioClient {
 
-
     @GetMapping("/{id}")
     UsuarioDTO findById(Long id);
 
