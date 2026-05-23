2e245334a14d6ae44f2b262688ffad929149f758
Nicolas <nico.mv1998@gmail.com>
2026-05-23 01:39:41 -0400

    creando el exceptions

---
+++ match-service/src/main/java/com/example/match_service/exceptions/MatchExceptions.java
@@ -0,0 +1,7 @@
+package com.example.match_service.exceptions;
+
+public class MatchExceptions extends RuntimeException {
+    public MatchExceptions(String message) {
+        super(message);
+    }
+}
