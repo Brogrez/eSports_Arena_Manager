9b36691d74b4fd2bc8f11de101835a89d5b022a8
Jose <jj.hernandez@duocuc.cl>
2026-05-22 20:52:39 -0400

    Se agrega las anotaciones faltantes

--- auth-service/src/main/java/eSports_Arena_Manager/auth_service/models/dtos/UsuarioDTO.java
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/models/dtos/UsuarioDTO.java
@@ -1,7 +1,16 @@
 package eSports_Arena_Manager.auth_service.models.dtos;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class UsuarioDTO {
 
     private Long idUsuario;
