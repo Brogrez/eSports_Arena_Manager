5098a1c389c4b3387a2f69c5556472650c33ebd1
Jose <jj.hernandez@duocuc.cl>
2026-05-22 21:21:56 -0400

    se agrega findByUsuarioId

--- auth-service/src/main/java/eSports_Arena_Manager/auth_service/repositories/CuentaRepository.java
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/repositories/CuentaRepository.java
@@ -4,11 +4,12 @@ import eSports_Arena_Manager.auth_service.models.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
 
     Optional<Cuenta> findByCorreo(String correo);
-
+    List<Cuenta> findByUsuarioId(Long idUsuario);
 }
