e8805f550a62f618eafe2943b2a1105d0c4567b8
Jose <jj.hernandez@duocuc.cl>
2026-05-22 21:20:59 -0400

    se agrega PathVariable faltante

--- auth-service/src/main/java/eSports_Arena_Manager/auth_service/clients/UsuarioClient.java
+++ auth-service/src/main/java/eSports_Arena_Manager/auth_service/clients/UsuarioClient.java
@@ -3,11 +3,12 @@ package eSports_Arena_Manager.auth_service.clients;
 import eSports_Arena_Manager.auth_service.models.dtos.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "user-service", url = "localhost:8000/api/v1/users")
 public interface UsuarioClient {
 
     @GetMapping("/{id}")
-    UsuarioDTO findById(Long id);
+    UsuarioDTO findById(@PathVariable Long id);
 
 }
