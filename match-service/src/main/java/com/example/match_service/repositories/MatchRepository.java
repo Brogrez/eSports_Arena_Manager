d5e58022c378f277dd5463843215783ac67b21e6
Nicolas <nico.mv1998@gmail.com>
2026-05-23 12:47:24 -0400

    creando repository

---
+++ match-service/src/main/java/com/example/match_service/repositories/MatchRepository.java
@@ -0,0 +1,15 @@
+package com.example.match_service.repositories;
+
+import com.example.match_service.models.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findByTourId(Long tourId);
+    List<Match> findByEstado(String estado);
+    List<Match> findByRound(String round);
+    boolean existsByABR(Long participanteAId, Long participanteBId, String round);
+}
