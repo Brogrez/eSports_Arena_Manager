e7f1050c5f15cb340a11f3f27460ad4fcdee992b
Jose <jj.hernandez@duocuc.cl>
2026-05-24 02:38:36 -0400

    se realiza PremioClient con su conexion correspondiente

---
+++ notification-service/src/main/java/com/duoc/notification_service/clients/PremioClient.java
@@ -0,0 +1,14 @@
+package com.duoc.notification_service.clients;
+
+import com.duoc.notification_service.models.dtos.PremioDTO;
+import com.duoc.notification_service.models.dtos.ResultadoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "prize-service", url = "localhost:8010/api/v1/prizes")
+public interface PremioClient {
+
+    @GetMapping("/{id}")
+    PremioDTO findbyid(@PathVariable Long id);
+}
