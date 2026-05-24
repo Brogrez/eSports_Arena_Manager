c461373e1710392ecdcf73308774b6ad019d0f0e
Jose <jj.hernandez@duocuc.cl>
2026-05-24 02:38:47 -0400

    se realiza RegistroClient con su conexion correspondiente

---
+++ notification-service/src/main/java/com/duoc/notification_service/clients/RegistroClient.java
@@ -0,0 +1,13 @@
+package com.duoc.notification_service.clients;
+
+import com.duoc.notification_service.models.dtos.InscripcionDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "registration-service", url = "localhost:8007/api/v1/inscripciones")
+public interface RegistroClient {
+
+    @GetMapping("/{id}")
+    InscripcionDTO findbyid(@PathVariable Long id);
+}
