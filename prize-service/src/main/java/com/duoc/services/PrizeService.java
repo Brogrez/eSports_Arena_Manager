3b5d44b5eb4729eeac9a94b7a7c2de6951c23149
Felipe Sánchez <sanchez.felipe.r@gmail.com>
2026-05-24 00:17:52 -0400

    feat: definir firmas de metodos en PrizeService segun requerimientos de negocio

--- prize-service/src/main/java/com/duoc/services/PrizeService.java
+++ prize-service/src/main/java/com/duoc/services/PrizeService.java
@@ -1,4 +1,15 @@
-package eSports_Arena_Manager.prize_service.services;
+package com.duoc.services;
+
+import com.duoc.models.Prize;
+import java.util.List;
 
 public interface PrizeService {
+    List<Prize> findAll();
+    Prize findById(Long id);
+    List<Prize> findByName(String name);
+    List<Prize> findByEstado(String estado);
+    List<Prize> findByTorneoId(Long torneoId);
+    Prize save(Prize prize);
+    void deleteById(Long id);
+    Prize updateById(Long id, Prize prize);
 }
