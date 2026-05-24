c89962b758428b3a753444d457256a88312211a0
Nicolas <nico.mv1998@gmail.com>
2026-05-23 23:15:32 -0400

    creando service y el impl

--- match-service/src/main/java/com/example/match_service/clients/RegistrationClient.java
+++ match-service/src/main/java/com/example/match_service/clients/RegistrationClient.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @FeignClient(name = "registration-service", url = "http://localhost:8005/api/v1/inscripciones")
 public interface RegistrationClient {
     @GetMapping("/{id}")
