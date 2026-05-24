c503af60d2c53c62913c8d4cd8465bc85e61b79c
Felipe Sánchez <sanchez.felipe.r@gmail.com>
2026-05-24 00:08:06 -0400

    feat: implementar clase Audit siguiendo estrictamente la estructura del grupo con @PrePersist

--- prize-service/src/main/java/com/duoc/models/Audit.java
+++ prize-service/src/main/java/com/duoc/models/Audit.java
@@ -1,4 +1,43 @@
-package eSports_Arena_Manager.prize_service.models;
+package com.duoc.models; // Tu ruta física actual
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Audit {
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    /**
+     * Este método se ejecuta automáticamente una vez que el objeto es creado
+     */
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    /**
+     * Este método se ejecuta automáticamente cuando se realiza cualquier actu
+     * lización del objeto que se encuentra asociado.
+     */
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
 }
