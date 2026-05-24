52404f07cafb6f1355e352579a46584bfcbf3934
Jose <jj.hernandez@duocuc.cl>
2026-05-24 02:26:35 -0400

    se crea SancionClient con sus conexiones correspondientes

---
+++ notification-service/src/main/java/com/duoc/notification_service/clients/SancionClient.java
@@ -0,0 +1,14 @@
+package com.duoc.notification_service.clients;
+
+import com.duoc.notification_service.models.dtos.SancionDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "sanction-service", url = "localhost:8009/api/v1/sanctions")
+public interface SancionClient {
+
+    @GetMapping
+    SancionDTO findbyid(@PathVariable Long id);
+
+}
