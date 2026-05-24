8ac72b31201547fb21bd7ac087dc3206e9a662a0
Jose <jj.hernandez@duocuc.cl>
2026-05-23 23:47:25 -0400

    se crea EquipoDTO con sus respectivos atributos

---
+++ registration-service/src/main/java/com/duoc/registration_service/models/dtos/EquipoDTO.java
@@ -0,0 +1,22 @@
+package com.duoc.registration_service.models.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+public class EquipoDTO {
+
+    private Long equipoId;
+    private String nombreEquipo;
+    private String juegoPrincipal;
+    private String estado;
+    private Long juegoPrincipalId;
+    private Long capitanId;
+
+}
