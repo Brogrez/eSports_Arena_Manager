d342a6c99e0771cea5816b880e2fef0208a9128f
Felipe Sánchez <sanchez.felipe.r@gmail.com>
2026-05-25 08:11:26 -0400

    feat: implementar PrizeServiceImpl con validacion de torneo externa via OpenFeign

--- prize-service/src/main/java/com/duoc/prize_service/services/PrizeService.java
+++ prize-service/src/main/java/com/duoc/prize_service/services/PrizeService.java
@@ -1,15 +1,13 @@
 package com.duoc.prize_service.services;
 
-import com.duoc.models.Prize;
+import com.duoc.prize_service.models.Prize;
+import com.duoc.prize_service.models.dtos.PrizeSaveDTO;
 import java.util.List;
 
 public interface PrizeService {
     List<Prize> findAll();
     Prize findById(Long id);
-    Prize findByName(String name); //
-    List<Prize> findByEstado(String estado);
     List<Prize> findByTorneoId(Long torneoId);
-    Prize save(Prize prize);
-    void deleteById(Long id);
-    Prize updateById(Long id, Prize prize);
+    Prize save(PrizeSaveDTO prizeSaveDTO);
+    void delete(Long id);
 }
