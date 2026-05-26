677af94d5f9807c5e7937a8d416c508dd7fae0b2
Felipe Sánchez <sanchez.felipe.r@gmail.com>
2026-05-25 20:15:39 -0400

    refactor: integrar RankingClient y fixear impl

--- prize-service/src/main/java/com/duoc/prize_service/services/PrizeService.java
+++ prize-service/src/main/java/com/duoc/prize_service/services/PrizeService.java
@@ -1,14 +1,13 @@
 package com.duoc.prize_service.services;
 
 import com.duoc.prize_service.models.Prize;
-import com.duoc.prize_service.models.dtos.PrizeSaveDTO;
 import java.util.List;
 
 public interface PrizeService {
-    List<PrizeSaveDTO> findAll();
+    List<Prize> findAll();
     Prize findById(Long id);
-    List<Prize> findByTorneoId(Long torneoId);
     Prize save(Prize prize);
     void deleteById(Long id);
-    Prize update(Long id, PrizeSaveDTO prizeSaveDTO);
+    Prize updateById(Long id, Prize prize);
+    List<Prize> findByTorneoId(Long torneoId);
 }
--- prize-service/src/main/java/com/duoc/prize_service/services/PrizeServiceImpl.java
+++ prize-service/src/main/java/com/duoc/prize_service/services/PrizeServiceImpl.java
@@ -1,25 +1,16 @@
 package com.duoc.prize_service.services;
 
 import com.duoc.prize_service.clients.TournamentClient;
-import com.duoc.prize_service.exceptions.PrizeException;
+import com.duoc.prize_service.clients.RankingClient;
 import com.duoc.prize_service.models.Prize;
-import com.duoc.prize_service.models.dtos.PrizeSaveDTO;
-import com.duoc.prize_service.models.dtos.TournamentDTO;
 import com.duoc.prize_service.repositories.PrizeRepository;
-import com.duoc.prize_service.services.PrizeService;
 import feign.FeignException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
 @Service
-@RequiredArgsConstructor
-@Slf4j
 public class PrizeServiceImpl implements PrizeService {
 
     @Autowired
@@ -28,79 +19,66 @@ public class PrizeServiceImpl implements PrizeService {
     @Autowired
     private TournamentClient tournamentClient;
 
+    @Autowired
+    private RankingClient rankingClient;
+
+    @Transactional(readOnly = true)
     @Override
-    public List<PrizeSaveDTO> findAll() {
-        return prizeRepository.findAll().stream().map(p ->{
-          PrizeSaveDTO prizeSaveDTO = new PrizeSaveDTO();
-          prizeSaveDTO.setMonto(p.getMonto());
-
-          prizeSaveDTO.setTorneoId(p.getTorneoId());
-          try{
-              TournamentDTO tournamentDTO = tournamentClient.findById(p.getTorneoId());
-
-            }catch(FeignException e){
-              e.getMessage();
-          }
-          TournamentDTO tournamentDTO = new TournamentDTO();
-          tournamentDTO.setTorneoId(p.getTorneoId());
-          prizeSaveDTO.setTorneoId(tournamentDTO.getTorneoId());
-          prizeSaveDTO.setNombre(p.getNombre());
-          prizeSaveDTO.setEstado(p.getEstado());
-          return prizeSaveDTO;
-        }).toList();
-
-
+    public List<Prize> findAll() {
+        return this.prizeRepository.findAll();
     }
 
-    @Override
     @Transactional(readOnly = true)
+    @Override
     public Prize findById(Long id) {
-        return prizeRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Premio no encontrado con el ID: " + id));
+        return this.prizeRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("EL PREMIO NO EXISTE")
+        );
     }
 
-    @Override
     @Transactional(readOnly = true)
+    @Override
     public List<Prize> findByTorneoId(Long torneoId) {
-        return prizeRepository.findByTorneoId(torneoId).stream().toList();
+        return this.prizeRepository.findByTorneoId(torneoId);
     }
 
-    @Override
     @Transactional
-
+    @Override
     public Prize save(Prize prize) {
         try {
-            TournamentDTO torneoExterno = tournamentClient.findById(prize.getTorneoId());
+            this.tournamentClient.findById(prize.getTorneoId());
         } catch (FeignException e) {
-            throw new PrizeException("Torneo no encontrado con el ID: " + prize.getTorneoId());
+            throw new RuntimeException("EL TORNEO NO EXISTE");
         }
 
-            prize.setNombre(prize.getNombre());
-            prize.setMonto(prize.getMonto());
-            prize.setTorneoId(prize.getTorneoId());
-            prize.setEstado(prize.getEstado());
-
-            return prizeRepository.save(prize);
+        try {
+            this.rankingClient.findByTorneoId(prize.getTorneoId());
+        } catch (FeignException e) {
+            throw new RuntimeException("EL RANKING DEL TORNEO NO EXISTE");
         }
 
-    @Override
-    public void deleteById(Long id) {
-        if (!prizeRepository.existsById(id)) {
-            throw new RuntimeException("Premio no encontrado con el ID: " + id);
-        }
-        prizeRepository.deleteById(id);
+        prize.setEstado("ASIGNADO");
+        return this.prizeRepository.save(prize);
     }
 
-
+    @Transactional
     @Override
-    public Prize update(Long id, PrizeSaveDTO prizeSaveDTO) {
-        return this.prizeRepository.findById(id).map(prize -> {
-            prize.setNombre(prizeSaveDTO.getNombre());
-            prize.setMonto(prizeSaveDTO.getMonto());
-            prize.setEstado(prizeSaveDTO.getEstado());
-            return this.prizeRepository.save(prize);
+    public Prize updateById(Long id, Prize prize) {
+        return this.prizeRepository.findById(id).map(p -> {
+            p.setNombre(prize.getNombre());
+            p.setMonto(prize.getMonto());
+            p.setTorneoId(prize.getTorneoId());
+            p.setEstado(prize.getEstado());
+            return this.prizeRepository.save(p);
         }).orElseThrow(
-                () -> new RuntimeException("Premio no encontrado con el ID: " + id)
+                () -> new RuntimeException("EL PREMIO NO EXISTE")
         );
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(Long id) {
+        Prize prize = this.findById(id);
+        this.prizeRepository.delete(prize);
     }
 }
